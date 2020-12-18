882c2348b116a664bd3f2b5991584be793d501e9
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-18 16:42:22 +0100

    Fix bug: add ingeretence from models.Model

--- auctions/admin.py
+++ auctions/admin.py
@@ -4,9 +4,11 @@ from .models import User, Listing, Bid, Comment
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Listing)
 
-class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "price", "creation_date")
+# class ListingAdmin(admin.ModelAdmin):
+#     list_display = ("id", "title", "price", "creation_date")
 
-# admin.site.register(Bid)
-# admin.site.register(Comment)
+
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -5,14 +5,17 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Listing():
+class Listing(models.Model):
     title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     creation_date = models.DateTimeField()
 
-class Bid():
+    def __str__(self):
+        return f"{ self.title } { self.price }"
+
+class Bid(models.Model):
     pass
 
-class Comment():
+class Comment(models.Model):
     pass
 
