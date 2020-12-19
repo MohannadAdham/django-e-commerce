0e77160ede2cd2acacf0f9d3b47710f9291a6183
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-19 06:49:04 +0100

    Implement the classes Listing, Bid and Comment and define their display in admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import User, Listing, Bid, Comment
 
 # Register your models here.
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "price", "creation_date")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "listing", "amount", "bid_date")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "listing", "content")
+
 admin.site.register(User)
-admin.site.register(Listing)
-
-# class ListingAdmin(admin.ModelAdmin):
-#     list_display = ("id", "title", "price", "creation_date")
-
-
-admin.site.register(Bid)
-admin.site.register(Comment)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -3,19 +3,31 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
 
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{ self.title } { self.price }"
 
 class Bid(models.Model):
-    pass
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    bid_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"{ self.user.first_name } { self.amount }$ { self.bid_date }"
 
 class Comment(models.Model):
-    pass
+    content = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="comments")
 
+    def __str__(self):
+        return f"{ self.content } by { self.user.first_name }"
